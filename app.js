9528578a4bf5e785d7bb1dd2d318823852cb3a58
jplatasCuracao <jplatas@icuracao.com>
2018-02-12 13:10:26 -0800

    Getting data from request. Refactor post and get functions.

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs');
 
 // parse incoming requests
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 // serve static files from /public
 app.use('/static',express.static(path.join(__dirname, 'public')));
--- routes/profile.js
+++ routes/profile.js
@@ -1,14 +1,54 @@
 /*
   profile router
 */
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var profileRouter = express.Router();
+var request = require('request'); //ajax call
 
 //profile page
-router.get('/',function (req, res, next){
-    res.render('profile', {title: 'Profile', condition: false});
+//GET
+profileRouter.get('/', function(req, res, next) {
+  var username = req.query.username; //get username form post
+  handleProfileRender(username,res);
 });
+//POST
+profileRouter.post('/', function(req, res, next) {
+  var username = req.body.username; //get username form post
+  handleProfileRender(username,res);
+});
+//handle the rending of the page
+function handleProfileRender(username,res){
+  //load profile
+  loadProfile(username, function(error, response, body) {
 
+    if(body == "Not found"){
+      res.render('errors/profileNotFound', {
+        title: 'Profile',
+        username: username,
+        condition: false
+      });
+    }else{
+      var profile = JSON.parse(body);
+      res.render('profile', {
+        title: 'Profile',
+        profile: profile,
+        condition: false
+      });
+    }
 
+  });
+}
 
-module.exports = router;
+//fuction to get data for user
+function loadProfile(username, callback) {
+  username = encodeURI(username.toLowerCase().trim());//clean data
+  var url = "https://teamtreehouse.com/"+username+".json";
+  request(url, function(error, response, body) {
+    //console.log('error:', error); // Print the error if one occurred
+    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('body:', body); // Print the HTML for the Google homepage.
+    callback(error, response, body);
+  });
+}
+
+module.exports = profileRouter;
