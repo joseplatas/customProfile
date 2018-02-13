39b40bc3037c58fe24bea6f80ade507311abb4ff
jplatasCuracao <jplatas@icuracao.com>
2018-02-12 16:25:36 -0800

    Finish inital build of the project

--- routes/profile.js
+++ routes/profile.js
@@ -3,43 +3,123 @@
 */
 var express = require('express');
 var profileRouter = express.Router();
+var hbs = require('handlebars');
 var request = require('request'); //ajax call
 
 //profile page
 //GET
 profileRouter.get('/', function(req, res, next) {
   var username = req.query.username; //get username form post
-  handleProfileRender(username,res);
+  if(username){
+    handleProfileRender(username,res);
+  }else{
+      res.render('index', {title: 'TeamTreehouse Profile', missingUser: true, });
+  }
+
 });
 //POST
 profileRouter.post('/', function(req, res, next) {
   var username = req.body.username; //get username form post
-  handleProfileRender(username,res);
+  if(username){
+    handleProfileRender(username,res);
+  }else{
+      res.render('index', {title: 'TeamTreehouse Profile', missingUser: true, });
+  }
 });
-//handle the rending of the page
+
+//handle bars helper function
+//NOTE: //build the html for the chart
+hbs.registerHelper('chart_builder', function(points){
+  //sort the points
+  var arr_points = sortObject(points)
+  var total_points = arr_points[1].value;
+
+  var html ="";
+  //container for POINT_LIST
+  var points_list = "<div class='points'>";
+  points_list += "<h2>Topics</h2>";
+  points_list += '<ul>';
+  //container for POINTS_CHART
+  var points_chart = "<div class='points_chart'>";
+  points_chart += "<h2>Points Chart</h2>";
+  points_chart += "<div class='chart_container'>";
+  var p = "";
+  //buildin the content
+  for (var i = 0; i < arr_points.length; i++) {
+    p = arr_points[i];
+    console.log(p);
+    // skip courses with zero points
+    if(p.value == 0 || p.key == "total"){
+      continue;
+    }
+    points_list += "<li>"+p.key+": <span>"+p.value+"</span></li>";
+    //bar
+    var bar_class = p.key.toLowerCase().split(' ').join('');
+
+    var bar_width = ((p.value /total_points) * 100).toFixed(2);
+    points_chart += "<div title='"+p.key+"' style='width: "+bar_width+"%;' class='bar "+bar_class+"'>"+p.key+"</div>";
+  }
+
+  //POINT LIST
+  points_list += "</ul>";
+  points_list += '</div>';
+  //POINTS_CHART
+  points_chart += "</div>";
+  points_chart += "</div>";
+
+
+  html = points_list + points_chart;
+  return html;
+});
+//string number seperated by commas
+hbs.registerHelper('number_with_commas', function(x){
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+});
+
+hbs.registerHelper('get_year',function(){
+  var d = new Date();
+  return d.getFullYear();
+});
+
+//NOTE: helper function for chart_builder
+function sortObject(obj) {
+    var arr = [];
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            arr.push({
+                'key': prop,
+                'value': obj[prop]
+            });
+        }
+    }
+    arr.sort(function(a, b) { return  b.value - a.value ; });
+    return arr; // returns array
+}
+
+
+//Refactor functions: handle the rendering of the page
 function handleProfileRender(username,res){
   //load profile
   loadProfile(username, function(error, response, body) {
 
     if(body == "Not found"){
-      res.render('errors/profileNotFound', {
+      res.render('index', {
         title: 'Profile',
         username: username,
-        condition: false
+        noFoundUser: true
       });
     }else{
       var profile = JSON.parse(body);
       res.render('profile', {
         title: 'Profile',
-        profile: profile,
-        condition: false
+        profile: profile
       });
     }
 
   });
 }
 
-//fuction to get data for user
+//Ajax Call:get data of user
 function loadProfile(username, callback) {
   username = encodeURI(username.toLowerCase().trim());//clean data
   var url = "https://teamtreehouse.com/"+username+".json";
